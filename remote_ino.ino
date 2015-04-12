d2d221b3a8057898d10a4975388b98489f14e04d
Diehard073055 <eshanshafeeq073055@gmail.com>
2015-04-13 00:24:49 +0800

    add(remote_ino.ino)

---
+++ remote_ino.ino
@@ -0,0 +1,15 @@
+/*
+//===ROTORY ENCODER
+//===TIMELAPSE CONTROLLER
+//===SETUP
+  display.begin(SSD1306_SWITCHCAPVCC);
+ if(!B_SIG)pulse++;//forward
+ if(!A_SIG)pulse--;//forward
+//Initiate All Menu Variables
+		amount of times after a specified delay.
+	//INITIALISATION
+	//---User Input Variables
+                        d_setCur(0,0);
+                           }
+			//---Timelapse control functions
+					shutter = true;
