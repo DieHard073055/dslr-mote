766caa4a62432709e52904a565d9dea7c02803f9
Diehard073055 <eshanshafeeq073055@gmail.com>
2015-04-13 00:24:49 +0800

    add(remote.ino)

---
+++ remote.ino
@@ -0,0 +1,8 @@
+/*
+//=======MAIN PROGRAM
+//---Process User Input
+//===TIMELAPSE CONTROLLER
+	boolean paused = false; // Time paused
+	unsigned long elapsed =0; pause_t = 0; //Total Elapsed Time and Time when the paused began
+
+					start = millis();
