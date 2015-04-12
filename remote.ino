eb5aa25ccde1691c9ed8ce00d6d296a396ed0b9e
Diehard073055 <eshanshafeeq073055@gmail.com>
2015-04-13 02:19:12 +0800

    add(remote.ino)

--- remote.ino
+++ remote.ino
@@ -6,3 +6,11 @@
 	unsigned long elapsed =0; pause_t = 0; //Total Elapsed Time and Time when the paused began
 
 					start = millis();
+	Diehard's DSLR Remote
+//===SETUP
+/*
+//Initialise All Timelapse Variables
+
+	int shots=0; //Total Number of shots taken
+			//---Timelapse control functions
+					shutter = true;
